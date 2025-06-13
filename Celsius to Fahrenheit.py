700aca277f09d65f38af0e616944814c67846d18
David Estrada <135681618+david7zr@users.noreply.github.com>
2025-06-12 21:47:28 -0700

    Add files via upload

---
+++ Celsius to Fahrenheit.py
@@ -0,0 +1,5 @@
+#Python, a program to convert Celsius temps to Fahrenheit
+
+celsius = eval(input("What is the Celsius tempetura? "))
+fahrenheit = (9/5) * celsius + 32
+print("Tempeture is", fahrenheit, "degree Fahrenheit")
